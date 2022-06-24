5937a27d349e0c652d8bb54d541b139e638a2c10
Brayan-724 <bjqf0724@gmail.com>
2022-06-23 20:32:50 -0700

    Adapt CanvasManager for multiples canvas

--- src/managers/CanvasManger.ts
+++ src/managers/CanvasManger.ts
@@ -1,54 +1,80 @@
 import { Axis, Vec2 } from "@utils";
 
 export class CanvasManager {
-  private static _canvas: HTMLCanvasElement;
-  private static _ctx: CanvasRenderingContext2D;
+  private static _activeCanvas: CanvasManager | null = null;
+
+  private static _getActiveCanvas(): CanvasManager {
+    if (CanvasManager._activeCanvas === null)
+      throw new Error("No canvas active");
+
+    return CanvasManager._activeCanvas;
+  }
 
   public static get canvas(): HTMLCanvasElement {
-    return CanvasManager._canvas;
+    return CanvasManager._getActiveCanvas().canvas;
   }
 
   public static get ctx(): CanvasRenderingContext2D {
-    return CanvasManager._ctx;
+    return CanvasManager._getActiveCanvas().ctx;
   }
 
-  public static createCanvas(width: number, height: number): HTMLCanvasElement {
+  public static createCanvas(width: number, height: number): CanvasManager {
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
-    return canvas;
-  }
-
-  public static init(canvas: HTMLCanvasElement): void {
-    CanvasManager._canvas = canvas;
-    const ctx = canvas.getContext("2d");
-    if (ctx === null) {
-      throw new Error("Canvas not supported");
-    }
-    CanvasManager._ctx = ctx;
+    return new CanvasManager(canvas);
   }
 
   public static clear(): void {
-    CanvasManager._ctx.clearRect(
+    CanvasManager.ctx.clearRect(
       0,
       0,
-      CanvasManager._canvas.width,
-      CanvasManager._canvas.height
+      CanvasManager.canvas.width,
+      CanvasManager.canvas.height
     );
   }
 
   public static normalizeSize(n: number, axis: Axis): number {
-    const canvas = CanvasManager._canvas;
+    const current = CanvasManager._getActiveCanvas();
+    const canvas = CanvasManager.canvas;
+
     const scale = axis === Axis.X ? canvas.width : canvas.height;
-    const size = axis === Axis.X ? 1000 : 500;
+    const size = axis === Axis.X ? 1000 : 1000 / current.ratio;
+
     return (n / size) * scale;
   }
 
   public static normalizeSizes(vec: Vec2): Vec2 {
-    const canvas = CanvasManager._canvas;
+    const current = CanvasManager._getActiveCanvas();
+    const canvas = CanvasManager.canvas;
+    
     const scale = new Vec2(canvas.width, canvas.height);
-    const size = new Vec2(1000, 500);
+    const size = new Vec2(1000, 1000 / current.ratio);
 
     return vec.clone().div(size).mul(scale);
+  }
+
+  //* -------------------------------------------------- *
+  //* ------------------- Instance --------------------- *
+  //* -------------------------------------------------- *
+
+  public readonly ctx: CanvasRenderingContext2D;
+  public readonly ratio: number;
+
+  constructor(public readonly canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext("2d")!;
+    if (ctx === null) throw new Error("Canvas not supported");
+
+    this.ctx = ctx;
+
+    this.ratio = canvas.width / canvas.height;
+  }
+
+  activate(): void {
+    CanvasManager._activeCanvas = this;
+  }
+
+  deactivate(): void {
+    CanvasManager._activeCanvas = null;
   }
 }
